d4e44914e190088976e15eb88b28c3dd064ff717
Gbemileke Adeleke <electronus78@gmail.com>
2022-02-08 20:40:58 +0100

    Improved error detection and added new event

--- dist/sequential-loader.js
+++ dist/sequential-loader.js
@@ -3,23 +3,16 @@
  * Author: Gbemileke Adeleke (https://github.com/meshachviktor)
  * License: MIT
  */
-(function() {
+ (function() {
     "use strict";
     function SequentialLoader(URL, debug) {
-        if (URL === '' || URL === undefined) {
-            throw new Error('URL cannot be empty!');
-        }
-        if (!(URL.endsWith('.json'))) {
-            throw new Error('Resources file path must point to a json document!');
-        }
-        this._debug = debug;
         this.url = URL;
+        this._debug = debug;
+        this.errorEvent = null;
         this.stylesheets = null;
         this.fonts = null;
         this.scripts = null;
-        this.stylesheetsLength = null;
-        this.fontsLength = null;
-        this.scriptsLength = null;
+        this.resourceCount = null;
         this.batchOrder = null;
         this.nextBatch = 0;
         this.nextStylesheet = 0;
@@ -32,6 +25,7 @@
         this.fontsLoadedEvent = new Event('sl.fontsloaded');
         this.stylesheetsLoadedEvent = new Event('sl.stylesheetsloaded');
         this.scriptsLoadedEvent = new Event('sl.scriptsloaded');
+        this.resourceLoadedEvent = new Event('sl.resourceloaded');
         this.allResourcesLoadedEvent = new Event('sl.allresourcesloaded');
     }
 
@@ -80,6 +74,29 @@
         }
     }
 
+    SequentialLoader.prototype.detectError = function(event) {
+        var isScriptError = this.scripts.some(function(scriptObject) {
+            var regex = new RegExp(scriptObject.src);
+            if (regex.test(event.filename)) {
+                if (/SyntaxError/.test(event.message)) {
+                    event.preventDefault();
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        });
+        if (isScriptError) {
+            this.errorEvent = event;
+        }
+    }
+
+    SequentialLoader.prototype.throwErrorIfErrorDetected = function() {
+        if (this.errorEvent) {
+            throw new Error(`Failed to load file ${this.errorEvent.filename}`);
+        }
+    }
+
     SequentialLoader.prototype.loadStylesheet = function(stylesheetObject) {
         var element = document.createElement('link');
         var attributes = Object.keys(stylesheetObject);
@@ -92,13 +109,19 @@
     }
 
     SequentialLoader.prototype.loadNextStylesheet = function() {
-        if (this.nextStylesheet < this.stylesheetsLength) {
+        document.dispatchEvent(this.resourceLoadedEvent);
+        if (this.nextStylesheet < this.stylesheets.length) {
             this.loadStylesheet(this.stylesheets[this.nextStylesheet]);
         } else {
             this.allStylesheetLoaded = true;
             document.dispatchEvent(this.stylesheetsLoadedEvent);
             return;
         }
+        this.nextStylesheet++;
+    }
+
+    SequentialLoader.prototype.beginStylesheetLoading = function() {
+        this.loadStylesheet(this.stylesheets[this.nextStylesheet]);
         this.nextStylesheet++;
     }
 
@@ -111,7 +134,6 @@
         }
         font.load().then(function() {
             document.fonts.add(font);
-            this.nextFont++;
             this.loadNextFont();
         }.bind(this)).catch(function(error) {
             throw new Error(`Could not load font from source: ${fontObject['source']}`);
@@ -119,12 +141,20 @@
     }
 
     SequentialLoader.prototype.loadNextFont = function() {
-        if (this.nextFont < this.fontsLength) {
+        document.dispatchEvent(this.resourceLoadedEvent);
+        if (this.nextFont < this.fonts.length) {
             this.loadFont(this.fonts[this.nextFont]);
         } else {
             this.allFontsLoaded = true;
             document.dispatchEvent(this.fontsLoadedEvent);
+            return;
         }
+        this.nextFont++;
+    }
+
+    SequentialLoader.prototype.beginFontLoading = function() {
+        this.loadFont(this.fonts[this.nextFont]);
+        this.nextFont++;
     }
 
     SequentialLoader.prototype.loadScript = function(scriptObject) {
@@ -138,13 +168,21 @@
     }
 
     SequentialLoader.prototype.loadNextScript = function() {
-        if (this.nextScript < this.scriptsLength) {
+        this.throwErrorIfErrorDetected();
+        document.dispatchEvent(this.resourceLoadedEvent);
+        if (this.nextScript < this.scripts.length) {
             this.loadScript(this.scripts[this.nextScript]);
         } else {
             this.allScriptsLoaded = true;
             document.dispatchEvent(this.scriptsLoadedEvent);
             return;
         }
+        this.nextScript++;
+    }
+
+    SequentialLoader.prototype.beginScriptLoading = function() {
+        this.throwErrorIfErrorDetected();
+        this.loadScript(this.scripts[this.nextScript]);
         this.nextScript++;
     }
 
@@ -155,7 +193,6 @@
         if (this.isEmptyArray(stylesheetsArray)) {
             throw new Error("Stylesheets array cannot be empty!");
         }
-        this.stylesheetsLength = stylesheetsArray.length;
         this.stylesheets = stylesheetsArray;
     }
 
@@ -166,7 +203,6 @@
         if (this.isEmptyArray(scriptsArray)) {
             throw new Error("scripts array cannot be empty!");
         }
-        this.scriptsLength = scriptsArray.length;
         this.scripts = scriptsArray;
     }
 
@@ -177,8 +213,7 @@
         if (this.isEmptyArray(fontsArray)) {
             throw new Error("Fonts array cannot be empty!");
         }
-        this.fontsLength = fontsArray.length;
-        for (let i=0; i<this.fontsLength; i++) {
+        for (let i=0; i<fontsArray.length; i++) {
             if (!this.isObject(fontsArray[i])) {
                 throw new Error('Invalid font object!');
             }
@@ -189,11 +224,11 @@
     SequentialLoader.prototype.processBatch = function() {
         if (this.nextBatch < this.batchOrder.length) {
             if (this.batchOrder[this.nextBatch] === 'fonts') {
-                this.loadNextFont(this.fonts);
+                this.beginFontLoading();
             } else if (this.batchOrder[this.nextBatch] === 'stylesheets') {
-                this.loadNextStylesheet(this.stylesheets);
+                this.beginStylesheetLoading();
             } else if (this.batchOrder[this.nextBatch] === 'scripts') {
-                this.loadNextScript(this.scripts);
+                this.beginScriptLoading();
             } else {
                 throw new Error(`Invalid resource type '${item}'`);
             }
@@ -209,6 +244,7 @@
         document.addEventListener(this.fontsLoadedEvent.type, this.processBatch.bind(this));
         document.addEventListener(this.stylesheetsLoadedEvent.type, this.processBatch.bind(this));
         document.addEventListener(this.scriptsLoadedEvent.type, this.processBatch.bind(this));
+        window.addEventListener('error', this.detectError.bind(this));
         if (this._debug) {
             document.addEventListener(this.allResourcesLoadedEvent.type, function() {
                 console.log('Sequential loading of all application resources completed.');
@@ -227,21 +263,24 @@
 
     SequentialLoader.prototype.initLoading = function() {
         if (this.url === '' || this.url === undefined) {
-            throw new Error('URL cannot be empty!');
+            throw new Error('Resource list file path is not defined!');
         }
         if (!(this.url.endsWith('.json'))) {
-            throw new Error('Resources file path must point to a json document!');
+            throw new Error('Resource list file path must point to a json document!');
         }
         this.initEventHandlers();
         fetch(this.url).then(function(response) {
             if (response.ok) {
                 return response.text();
             } else {
-                throw new Error("Could not load resources file.");
+                throw new Error("Could not load resource list file.");
             }
         }).then(function(data) {
             if (this.isJSONString(data)) {
                 var resources = JSON.parse(data);
+                if (resources.fonts === undefined && resources.stylesheets === undefined && resources.scripts === undefined) {
+                    throw new Error(`seqentialLoader could not find any resource to load.`);
+                }
                 if (resources.order === undefined) {
                     var order = Object.keys(resources);
                     this.batchOrder = order;
@@ -251,17 +290,20 @@
                 this.batchOrder.forEach(function(item) {
                     if (item === 'fonts') {
                         this.initFontLoading(resources['fonts']);
+                        this.resourceCount += this.fonts.length;
                     } else if (item === 'stylesheets') {
                         this.initSylesheetLoading(resources['stylesheets']);
+                        this.resourceCount += this.stylesheets.length;
                     } else if (item === 'scripts') {
                         this.initScriptLoading(resources['scripts']);
+                        this.resourceCount += this.scripts.length;
                     } else {
                         throw new Error(`Invalid resource type '${item}'`);
                     }
                 }.bind(this));
                 document.dispatchEvent(this.allResourcesInitialisedEvent);
             } else {
-                throw new Error("Content of resources file does not contain a valid json string.");
+                throw new Error("Content of resources file does not contain a valid json text.");
             }
         }.bind(this));
     }
